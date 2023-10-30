f6c240da4bc29c25159f8066f9f8f090d15d83e1
benspector-mls <ben@marcylabschool.org>
2023-10-30 15:49:09 -0400

    can get username

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const prompt = require('prompt-sync')();
+
+// Helper Functions
+const askForName = () => {
+  console.log("Welcome!")
+  const name = prompt('What is your name? ');
+  console.log(`hello ${name}`);
+  return name;
+}
+
+// Main "runner" function
+const main = () => {
+  const username = askForName();
+
+}
+main();
