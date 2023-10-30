85c3fb649a8afe9aa2a4b7d7e2c0d179c82d5e48
benspector-mls <ben@marcylabschool.org>
2023-10-30 15:57:43 -0400

    foundation complete

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ const askForName = () => {
   return name;
 }
 
+const showOptions = () => {
+  console.log("Here are your options:")
+  console.log("1 - play a guessing game")
+  console.log("2 - words of wisdom")
+  console.log("3 - cheer you on!")
+  console.log("Any other key - exit")
+}
+
+const sayGoodbye = (name) => {
+  console.log(`See ya later ${name}`);
+}
+
 // Main "runner" function
 const main = () => {
   const username = askForName();
-
+  showOptions();
+  sayGoodbye(username);
 }
 main();
